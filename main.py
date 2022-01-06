e1de401e7007f7bbf9abe179367f4ee85eeb87e5
data087 <dbs08787@gmail.com>
2022-01-06 15:22:49 +0900

    incourage

--- main.py
+++ main.py
@@ -41,8 +41,32 @@ def card(rank, suit):
         raise Exception("Rank out of range")
 
 
-deck = [card(rank, suit) for rank in range(1, 14) for suit in (Club, Diamond, Heart, Spade)]
+def card2(rank, suit):
+    if rank == 1:
+        return AceCard('A', suit)
+    elif 2 <= rank < 11:
+        return NumberCard(str(rank), suit)
+    else: # else문은 확신할때만 사용할 것. card 함수가 더 좋음.
+        name = {11: 'J', 12: 'Q', 13: 'K'}[rank]
+        return FaceCard(name, suit) # too bad?
 
 
+def card3(rank, suit):
+    if rank == 1:
+        return AceCard('A', suit)
+    elif 2 <= rank < 11:
+        return NumberCard(str(rank), suit)
+    elif rank == 11:
+        return FaceCard('J', suit)
+    elif rank == 12:
+        return FaceCard('Q', suit)
+    elif rank == 13:
+        return FaceCard('K', suit)
+    else:
+        raise Exception("Rank out of range")
 
+def card4(rank, suit):
+    class_ = {1: AceCard, 11: FaceCard, 12: FaceCard, 13:FaceCard}.get(rank, NumberCard)
+    return class_(rank, suit)
 
+#deck2 = [card2(rank, suit) for rank in range(13) for suit in (Club, Diamond, Heart, Spade)]
